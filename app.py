53d183646880909340c2a833cad87c9f966d4f79
Michal <miketopolsky1@interia.pl>
2019-04-26 16:22:18 +0200

    basic flask setup

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from cat_api import CatApi
+from flask import Flask, render_template
 
 api = CatApi()
 breeds = api.get_breeds()
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
