c771e5964d0774048755217b02f2ce9c49807630
Michal <miketopolsky1@interia.pl>
2019-04-27 14:07:49 +0200

    added breeds subpage

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from cat_api import CatApi
 from flask import Flask, render_template
 
 api = CatApi()
-breeds = api.get_breeds()
+
 
 app = Flask(__name__)
 
@@ -11,5 +11,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/breeds')
+def breeds():
+    breeds = api.get_breeds()
+    return render_template('breeds.html', breeds=breeds)
+
 if __name__ == '__main__':
+    print('run')
     app.run(debug=True)
